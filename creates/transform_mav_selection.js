ee54eb876e61ebde3e199bc3b5aa617097bede63
buzzykiwi <31227146+buzzykiwi@users.noreply.github.com>
2023-03-03 12:01:12 +1300

    stub for a new data transformation action

---
+++ creates/transform_mav_selection.js
@@ -0,0 +1,60 @@
+'use_strict';
+const FluxxAPI = require('../fluxx_api');
+
+const perform = async (z, bundle) => {
+
+  return { dict: z.JSON.stringify(bundle.inputData.dict),
+    in: z.JSON.stringify(bundle.inputData.line_items)};
+
+};
+
+module.exports = {
+  key: 'transform_mav_selection',
+  noun: 'Transformation',
+  display: {
+    label: 'Transform a Multi-Item Selection',
+    description:
+      "Transform the list of selections for a multi-item field, by path or id. (line item enabled).",
+    hidden: false,
+    important: true,
+  },
+  operation: {
+    inputFields: [
+      {
+        key: 'line_items',
+        label: 'Line Items',
+        children: [
+          {
+            key: 'mav_creation_line_item',
+            label: '"Add List" Or "Add List By Id" Line Item',
+            type: 'string',
+            helpText: "Choose the input line item that contains the MAV creation string",
+            required: false,
+          },
+          {
+            key: 'id',
+            label: 'Record Id',
+            type: 'integer',
+            helpText: "Select the field containing the line item for the ids of the records.",
+            required: true,
+          }
+        ],
+      },
+      {
+        key: 'dict',
+        label: 'Transformation Dictionary',
+        type: 'string',
+        dict: true,
+        helpText:
+          "Specify the id *or* full path including delimiters on the left, and the new id or new full path on the right.",
+        required: true,
+        list: false,
+        altersDynamicFields: false,
+      },
+      
+    ],
+    perform: perform,
+    sample: { id: 30444, name: 'default' },
+    outputFields: [{ key: 'id', type: 'integer' }, { key: 'name' }],
+  },
+};

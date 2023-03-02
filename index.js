790c8a58ac847635f5efe8748d1c5338d2c29125
buzzykiwi <31227146+buzzykiwi@users.noreply.github.com>
2023-03-03 12:01:48 +1300

    support for new triggers and actions

--- index.js
+++ index.js
@@ -20,15 +20,19 @@ const getModelDocumentType = require("./triggers/model_document_type");
 const getModelDocumentSubType = require("./triggers/model_document_sub_type");
 const recordsSqlTrigger = require("./triggers/records_sql_trigger");
 const multiFieldsForModel = require("./triggers/multi_fields_for_model.js");
+const allFieldsForModel = require("./triggers/all_fields_for_model.js");
+const writeableFieldsForModel = require("./triggers/writeable_fields_for_model.js");
 
 // Creates
 const recordSqlSearch = require('./creates/record_sql_search.js');
 const recordsSqlSearch = require('./creates/records_sql_search.js');
 const updateFluxxRecordCreate = require('./creates/record_update_new.js');
+const updateFluxxRecordLineItemCreate = require('./creates/record_update_new_line_item.js');
 const uploadFileCreate = require('./creates/file_upload.js');
 const downloadFileCreate = require('./creates/file_download.js');
 const selectListManagement = require('./creates/select_list_management.js');
 const transactionCreate = require('./creates/transaction_new.js');
+const transformMavSelection = require('./creates/transform_mav_selection.js');
 
 // Other
 const hydrators = require('./hydrators');
@@ -56,15 +60,19 @@ module.exports = {
     [getModelDocumentSubType.key]: getModelDocumentSubType,
     [recordsSqlTrigger.key]: recordsSqlTrigger,
     [multiFieldsForModel.key]: multiFieldsForModel,
+    [allFieldsForModel.key]: allFieldsForModel,
+    [writeableFieldsForModel.key]: writeableFieldsForModel,
   },
   creates: {
     [updateFluxxRecordCreate.key]: updateFluxxRecordCreate,
+    [updateFluxxRecordLineItemCreate.key]: updateFluxxRecordLineItemCreate,
     [recordSqlSearch.key]: recordSqlSearch,
     [recordsSqlSearch.key]: recordsSqlSearch,
     [uploadFileCreate.key]: uploadFileCreate,
     [downloadFileCreate.key]: downloadFileCreate,
     [selectListManagement.key]: selectListManagement,
     [transactionCreate.key]: transactionCreate,
+    [transformMavSelection.key]: transformMavSelection,
   },
   searches: {
     [searchUser.key]: searchUser,
